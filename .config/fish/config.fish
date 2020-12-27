de5bf54fc07e702413045e497a2f1d442d89761e
lukasjoc <jochamlu@gmail.com>
2020-12-27 21:11:12 +0100

    tinker with set -x and set -U

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,42 +1,41 @@
 #!/usr/local/bin/fish
 
 set -U fish_greeting
-set -U EDITOR "nvim"
-set -U TERM "xterm-256color"
-set XDG_CACHE_HOME "$HOME/.cache"
-set XDG_DATA_HOME "$HOME/.local/share"
-set -U XDG_DATA_BIN "$HOME/.local/bin"
+set -x EDITOR "nvim"
+set -x TERM "xterm-256color"
+set -U XDG_CACHE_HOME "$HOME/.cache"
+set -U XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_DATA_BIN "$HOME/.local/bin"
 
 # Rust
-set RUST_SRC_PATH "$HOME/.cargo"
-set -U RUSTBIN "$HOME/.cargo/bin"
+set -x RUST_SRC_PATH "$HOME/.cargo"
+set -x RUSTBIN "$HOME/.cargo/bin"
 
 # Go and GoMods
-set -U GOPATH "$HOME/go"
-set -U GOBIN "$GOfish_user_paths/bin"
-set GO111MODULE "on"
-
-# Add programs to path
-set -U fish_user_paths "/usr/local/bin" "/usr/local/sbin" $XDG_DATA_BIN $RUSTBIN $GOBIN $fish_user_paths
+set -x GOPATH "$HOME/go"
+set -x GOBIN "$HOME/go/bin"
+set -U GO111MODULE "on"
 
 # LaTeX
-set -U TEXMFHOME "$HOME/Library/texmf"
+set -x TEXMFHOME "$HOME/Library/texmf"
 
 # Php
-set -U PHPBIN "$HOME/.config/composer/vendor/bin"
+set -x PHPBIN "$HOME/.config/composer/vendor/bin"
 
-set -U fish_user_paths \
+# Add programs to path
+set -x fish_user_paths \
   "/usr/local/bin" \
   "/usr/local/sbin" \
   $XDG_DATA_BIN \
   $RUSTBIN \
   $GOBIN \
   $TEXMFHOME \
-  $PHPBIN $fish_user_paths
+  $PHPBIN \
+  $fish_user_paths
 
 # homebrew no auto update
 if command -v brew &>/dev/null
-  set -U HOMEBREW_NO_AUTO_UPDATE 1;
+  set -x HOMEBREW_NO_AUTO_UPDATE 1;
 end
 
 # Essential aliases
